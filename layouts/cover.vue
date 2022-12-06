8273fb8528bd0550966776b4859f912bfd3eb21f
AnnyTerfect <1247470246@qq.com>
2022-12-06 13:36:32 +0800

    add nju logo

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -13,6 +13,8 @@ const style = computed(() => handleBackground(props.background, true))
 
 <template>
   <div class="slidev-layout cover" :style="style">
+    <img class="fixed left-6 top-6 w-25" src="../public/images/nju.png" />
+    <img class="fixed w-1/1 left-0 -bottom-3" src="../public/images/nju-bottom.png" />
     <div class="my-auto w-full">
       <slot />
     </div>
