b819fed22ca7eba2150fee42704472bda91c80bf
AnnyTerfect <1247470246@qq.com>
2023-03-01 11:03:15 +0800

    Add an SVG cnavas

---
+++ components/MyCanvas.vue
@@ -0,0 +1,34 @@
+<template>
+	<svg
+        :style="`fill: ${fill}; stroke: ${stroke};`"
+        class="absolute top-0 left-0 w-1/1 h-1/1"
+        viewBox="0 0 1600 900"
+        @click="click"
+    >
+        <defs>
+            <marker
+                id="arrow"
+                viewBox="0 0 10 10"
+                refX="5"
+                refY="5"
+                markerUnits="strokeWidth"
+                markerWidth="4"
+                markerHeight="4"
+                orient="auto">
+                <path d="M 0 0 L 10 5 L 0 10 z" />
+            </marker>
+        </defs>
+        <slot></slot>
+    </svg>
+</template>
+
+<script>
+	export default {
+		props: ['fill', 'stroke'],
+        methods: {
+            click(event) {
+                console.log(event)
+            }
+        }
+	}
+</script>
