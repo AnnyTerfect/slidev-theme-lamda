773507c800dfad88fc26c632cf34fa1084b6a196
AnnyTerfect <1247470246@qq.com>
2023-03-10 22:07:26 +0800

    MyCanvas and GCanvas components

--- components/MyCanvas.vue
+++ components/MyCanvas.vue
@@ -1,57 +1,71 @@
 <script setup>
-    import { ref, defineProps, computed, onMounted } from 'vue'
+import { ref, computed, defineProps } from 'vue'
 
-    const props = defineProps({
-        arrowFill: {
-            type: String,
-            default: 'none'
-        },
-        debug: {
-            type: Boolean,
-            default: false
-        }
-    })
+const svg = ref(null)
+const mousePosition = ref({ x: 0, y: 0 })
 
-    // SVG auto resize to page size
-    const svg = ref(null)
-    
-    // Mouse position
-    const mouse = ref({ x: 0, y: 0 })
-    function mousemove(event) {
-        mouse.value.x = event.offsetX
-        mouse.value.y = event.offsetY
+const width = computed(() => {
+    const [x, y, w, h] = props.viewBox.split(' ').map(Number)
+    return w
+})
+const height = computed(() => {
+    const [x, y, w, h] = props.viewBox.split(' ').map(Number)
+    return h
+})
+
+const props = defineProps({
+    debug: {
+        type: Boolean,
+        default: false,
+    },
+    viewBox: {
+        type: String,
+        required: true,
+    },
+})
+
+function handleMouseMove(event) {
+    mousePosition.value = {
+        x: event.offsetX / svg.value.clientWidth * width.value,
+        y: event.offsetY / svg.value.clientHeight * height.value,
     }
+}
 </script>
 
 <template>
-	<svg
+    <svg
         ref="svg"
-        class="absolute top-0 left-0 w-1/1 h-1/1"
-        :viewBox="svg ? `0 0 ${svg.clientWidth} ${svg.clientHeight}` : '0 0 0 0'"
-        @mousemove="mousemove"
+        :viewBox="viewBox"
+        @mousemove="handleMouseMove"
     >
-        <defs>
-            <marker
-                id="arrow"
-                viewBox="0 0 10 10"
-                refX="5"
-                refY="5"
-                markerUnits="strokeWidth"
-                markerWidth="4"
-                markerHeight="4"
-                orient="auto">
-                <path d="M 0 0 L 10 5 L 0 10 z" :fill="`${arrowFill}`" />
-            </marker>
-        </defs>
         <slot></slot>
         
-        <g v-if="debug">
-            <text>
-                <tspan x="1rem" y="1rem" fill="black" font-size="1rem">x = {{ mouse.x }}</tspan>
-                <tspan x="6rem" y="1rem" fill="black" font-size="1rem">y = {{ mouse.y }}</tspan>
+        <!--show mouse position-->
+        <g :visibility="debug ? 'visible' : 'hidden'">
+            <line
+                x1="0"
+                :y1="mousePosition.y"
+                :x2="width"
+                :y2="mousePosition.y"
+                stroke="red"
+                stroke-width="1"
+            />
+            <line
+                :x1="mousePosition.x"
+                y1="0"
+                :x2="mousePosition.x"
+                :y2="height"
+                stroke="red"
+                stroke-width="1"
+            />
+            <text
+                :x="mousePosition.x < width / 2 ? mousePosition.x + 5 : mousePosition.x - 5"
+                :y="mousePosition.y < height / 2 ? mousePosition.y + 5 : mousePosition.y - 10"
+                class="fill-red-500 text-0.5em mix-blend-diff"
+                :style="{ textAnchor: mousePosition.x < width / 2 ? 'start' : 'end', dominantBaseline: mousePosition.y < height / 2 ? 'hanging' : 'baseline' }"
+            >
+                {{ mousePosition.x.toFixed(2) }}, {{ mousePosition.y.toFixed(2) }}
             </text>
-            <line :x1="mouse.x" :x2="mouse.x" :y1="0" :y2="svg ? svg.clientHeight : 0" stroke="black"></line>
-            <line x1="0" :x2="svg ? svg.clientWidth : 0" :y1="mouse.y" :y2="mouse.y" stroke="black"></line>
         </g>
     </svg>
 </template>
