30d723bcd4c271d4706b39f1949a75036a687844
AnnyTerfect <1247470246@qq.com>
2023-05-04 22:41:53 +0800

    feat: add myrect component

---
+++ components/SVG/Shape/MyRect.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+  x1: {
+    type: Number,
+    required: true,
+  },
+  y1: {
+    type: Number,
+    required: true,
+  },
+  x2: {
+    type: Number,
+    required: true,
+  },
+  y2: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <path :d="`M ${x1} ${y1} L ${x2} ${y1} L ${x2} ${y2} L ${x1} ${y2} Z`" />
+</template>
